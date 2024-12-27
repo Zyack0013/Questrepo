c8cbcb4b116b762a8f9c98f5a0c4fc7ef56577c9
Ananthu Joshylal <83502747+Zyack0013@users.noreply.github.com>
2024-12-27 07:20:52 +0530

    Assignment 1

--- Quest Assignment 1/QA1Q02.c
+++ Quest Assignment 1/QA1Q02.c
@@ -4,3 +4,48 @@
 // A static variable to count the total number of prime numbers found.
 // Nested for loops for the prime-checking logic.
 // Key Concepts Covered: Type qualifiers (const), Storage classes (static), Looping (for).
+
+
+
+#include <stdio.h>
+
+void prime(int const N);
+
+
+int main(){
+
+    int const N=100;
+    prime(N);
+
+    return 0;
+
+}
+
+void prime(int const N){
+
+    static int count = 0; 
+    printf("Prime numbers between 1 and %d are : \n",N);
+
+    for(int i=2;i<=N;i++){
+
+        int isP = 1;
+
+        for(int j=2;j*j<=i;j++){
+
+            if(i%j==0){
+
+                isP = 0;
+                break;
+
+            }
+        }
+        if(isP){
+
+            printf("%d ",i);
+            count++;
+
+        }
+    }
+    printf("\n Total number of prime numbers : %d\n",count);
+}
+
