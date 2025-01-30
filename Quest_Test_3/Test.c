28b96ee0e2409f2b0b00774184cc8fdce0dc1d18
Ananthu Joshylal <83502747+Zyack0013@users.noreply.github.com>
2025-01-30 15:06:27 +0530

    Test 3(30/01/2025)

---
+++ Quest_Test_3/Test.c
@@ -0,0 +1,31 @@
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+	int n = 5,sum=0;
+	int *arr = (int *)malloc(n * sizeof(int));
+	
+	if(arr == NULL){
+
+		printf("Memoery allocation failed");
+
+	}
+	
+	for(int i = 0;i < n;i++){
+    	arr[i] = i + 15;
+	}
+	
+	printf("Array elements are : ");
+	for(int i = 0;i < n;i++){
+		printf("%d ",*(arr+i));
+	}
+	
+	for(int i = 0;i >= 0;i++){
+		arr[i] = i - 5;
+	}
+	
+	return 0;
+}
